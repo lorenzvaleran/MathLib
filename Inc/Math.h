136fb4b67ae3a4d1d7f3f1b7d02bb3e250681b44
Lorenz Valeran <lorenz.valeran@hotmail.com>
2017-07-05 13:15:21 +0200

    first commit

---
+++ Inc/Math.h
@@ -0,0 +1,20 @@
+/*
+ * Math.h
+ *
+ *  Created on: 3 juil. 2017
+ *      Author: lo
+ */
+
+#ifndef MATH_H_
+#define MATH_H_
+
+class Math {
+public:
+	Math();
+	virtual ~Math();
+
+	int add(int a, int b);
+	int sub(int a, int b);
+};
+
+#endif /* MATH_H_ */
---
+++ Src/Math.cpp
@@ -0,0 +1,26 @@
+/*
+ * Math.cpp
+ *
+ *  Created on: 3 juil. 2017
+ *      Author: lo
+ */
+
+#include "../Inc/Math.h"
+
+Math::Math() {
+	// TODO Auto-generated constructor stub
+}
+
+Math::~Math() {
+	// TODO Auto-generated destructor stub
+}
+
+int Math::add(int a, int b)
+{
+	return (a + b);
+}
+
+int Math::sub(int a, int b)
+{
+	return (a - b);
+}
